c1fa6683a2a562f53a7fbadd4c0659e53d5d4f5c
usamaelgendy <olgindy@mog.gov.sa>
2023-12-17 21:04:03 +0200

    Add webpack-hmr.config.js to Hot reload

--- webpack-hmr.config.js
+++ webpack-hmr.config.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const nodeExternals = require('webpack-node-externals');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
 
 module.exports = function (options, webpack) {
