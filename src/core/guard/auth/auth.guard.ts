dc94fdd855975acf310c2c2317c89c97dc3292e2
usamaelgendy <olgindy@mog.gov.sa>
2023-12-20 22:35:32 +0200

    Add Midlleware and Validation

---
+++ src/core/guard/auth/auth.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest() as Request;
+    console.log(request.headers);
+    return true;
+  }
+}
